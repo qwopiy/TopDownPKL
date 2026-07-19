21ff2314773afca06b37742b14a471560729e5fe
Bintang Khalik <bintangkhaliqq@gmail.com>
2026-07-19 20:38:22 +0700

    Add MainMenu

---
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void StartGame()
+    {
+        // go to game scene
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
